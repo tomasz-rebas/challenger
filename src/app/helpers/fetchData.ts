13d09ef3e9ee4f0f585fc2bfa171a6fb13489e5e
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2025-08-18 17:19:50 +0200

    Add a clarifying comment to the helper

--- src/app/helpers/fetchData.ts
+++ src/app/helpers/fetchData.ts
@@ -1,4 +1,12 @@
 export async function fetchData(apiRoute: string) {
+  /**
+   * In a real-world application, I wouldn't call an internal API route in a server component.
+   * Since the server has a direct access to the contents of the route, it wouldn't make sense.
+   *
+   * Nevertheless, for the purpose of the assignment, I'm going to assume the
+   * API route is just a part of the database mock - i.e. I'm going to treat
+   * it as an external route.
+   */
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/${apiRoute}`
   );
