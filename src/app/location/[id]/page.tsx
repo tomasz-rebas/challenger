5ad37bb36ed0495e73c827522b72f5becd7613e2
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2025-08-18 16:29:21 +0200

    Add the location specific details to the new page

--- src/app/location/[id]/page.tsx
+++ src/app/location/[id]/page.tsx
@@ -1,4 +1,7 @@
 import { notFound } from "next/navigation";
+import Link from "next/link";
+import Image from "next/image";
+import { ArrowLeft } from "lucide-react";
 import { Params } from "@/app/types";
 import { fetchData } from "@/app/helpers/fetchData";
 
@@ -14,6 +17,25 @@ export default async function Location({ params }: Props) {
   }
 
   const { location } = data;
+  const { imageUrl, name, city, country } = location;
 
-  return <div>Location specific details: {location.name}</div>;
+  return (
+    <div className="flex flex-col space-y-4">
+      <Link href="/" className="flex gap-1">
+        <ArrowLeft />
+        <span>To homepage</span>
+      </Link>
+      <Image
+        className="object-cover"
+        src={imageUrl}
+        width={400}
+        height={280}
+        alt={name}
+      />
+      <h2>{name}</h2>
+      <h3>
+        {city}, {country}
+      </h3>
+    </div>
+  );
 }
