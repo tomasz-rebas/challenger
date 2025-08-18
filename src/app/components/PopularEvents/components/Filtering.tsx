db91142b90fafb33df77cb74c463fe0923771895
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2025-08-18 15:25:20 +0200

    Add a control for filtering results by location

---
+++ src/app/components/PopularEvents/components/Filtering.tsx
@@ -0,0 +1,40 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { EventLocation } from "@/app/types";
+import { Dispatch, SetStateAction } from "react";
+
+interface Props {
+  locations: EventLocation[];
+  selectedLocation: string;
+  setSelectedLocation: Dispatch<SetStateAction<string>>;
+}
+
+export default function Filtering({
+  locations,
+  selectedLocation,
+  setSelectedLocation,
+}: Props) {
+  return (
+    <Select value={selectedLocation} onValueChange={setSelectedLocation}>
+      <SelectTrigger className="w-auto">
+        <SelectValue placeholder="Filter by location" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="all">All locations</SelectItem>
+        {locations.map((location: EventLocation) => (
+          <SelectItem
+            key={`location_filter_${location.id}`}
+            value={`${location.id}`}
+          >
+            {location.name}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+}
--- src/app/components/PopularEvents/index.tsx
+++ src/app/components/PopularEvents/index.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import { useState } from "react";
 import { Calendar } from "lucide-react";
 import { EventLocation, PopularEvent } from "@/app/types";
 import EventsGrid from "./components/EventsGrid";
+import Filtering from "./components/Filtering";
 
 interface Props {
   events: PopularEvent[];
@@ -10,11 +12,18 @@ interface Props {
 }
 
 export function PopularEvents({ events, locations }: Props) {
+  const [selectedLocation, setSelectedLocation] = useState<string>("all");
+
   return (
     <>
       <h1 className="text-xl flex items-center gap-2">
         <Calendar /> Popular events
       </h1>
+      <Filtering
+        locations={locations}
+        selectedLocation={selectedLocation}
+        setSelectedLocation={setSelectedLocation}
+      />
       <EventsGrid events={events} locations={locations} />
     </>
   );
