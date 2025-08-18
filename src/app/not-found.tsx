e91f820b770a6d0a7f415e29b6d9d92276d7462c
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2025-08-18 16:36:28 +0200

    Styling tweaks for the 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,10 @@
 export default function NotFound() {
-  return <div>Not found</div>;
+  return (
+    <div className="flex h-[80vh]">
+      <div className="m-auto text-center">
+        <h1 className="text-4xl mb-6">404</h1>
+        <p>Page not found</p>
+      </div>
+    </div>
+  );
 }
