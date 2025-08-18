de296081215d4c1d3f494efc3da38a2ab35b1989
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2025-08-18 16:38:38 +0200

    Add error.tsx to handle unexpected errors

---
+++ src/app/error.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function Error() {
+  return (
+    <div className="flex h-[80vh]">
+      <div className="m-auto text-center">
+        <h1 className="text-4xl mb-6">Whoops!</h1>
+        <p>Unexpected error occured</p>
+      </div>
+    </div>
+  );
+}
