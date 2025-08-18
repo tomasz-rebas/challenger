0a96382ac4707ea79b516e2f697899b3fcb4ac9d
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2025-08-18 14:39:50 +0200

    Transform event cards into links pointing to specific events (i.e. their pages)

--- src/app/components/PopularEvents/index.tsx
+++ src/app/components/PopularEvents/index.tsx
@@ -3,6 +3,7 @@
 import { Calendar } from "lucide-react";
 import Image from "next/image";
 import { PopularEvent } from "@/app/types";
+import Link from "next/link";
 
 interface Props {
   events: PopularEvent[];
@@ -16,25 +17,25 @@ export function PopularEvents({ events }: Props) {
       </h1>
       <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-3">
         {events.map((event) => (
-          <div
-            key={`event_card_${event.id}`}
-            className="relative aspect-video rounded-lg overflow-hidden"
-          >
-            <div className="absolute inset-x-0 bottom-0 p-2">
-              <h1 className="text-sm text-primary-foreground">{event.name}</h1>
-              <p className="text-xs text-secondary-foreground">
-                {event.locationId} - {event.date}
-              </p>
+          <Link href={`/event/${event.id}`} key={`event_card_${event.id}`}>
+            <div className="relative aspect-video rounded-lg overflow-hidden">
+              <div className="absolute inset-x-0 bottom-0 p-2">
+                <h1 className="text-sm text-primary-foreground">
+                  {event.name}
+                </h1>
+                <p className="text-xs text-secondary-foreground">
+                  {event.locationId} - {event.date}
+                </p>
+              </div>
+              <Image
+                className="object-cover h-full w-full"
+                src={event.imageUrl}
+                width={320}
+                height={200}
+                alt={event.name}
+              />
             </div>
-
-            <Image
-              className="object-cover h-full w-full"
-              src={event.imageUrl}
-              width={320}
-              height={200}
-              alt={event.name}
-            />
-          </div>
+          </Link>
         ))}
       </div>
     </>
