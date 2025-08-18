a4239dea92a9198d4e2708539215e14c20a9e5df
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2025-08-18 14:35:54 +0200

    'Popular events' header should not be a part of the events grid

--- src/app/components/PopularEvents/index.tsx
+++ src/app/components/PopularEvents/index.tsx
@@ -10,32 +10,33 @@ interface Props {
 
 export function PopularEvents({ events }: Props) {
   return (
-    <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-3">
-      <h1 className="text-xl md:col-span-3 flex items-center gap-2">
+    <>
+      <h1 className="text-xl flex items-center gap-2">
         <Calendar /> Popular events
       </h1>
+      <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-3">
+        {events.map((event) => (
+          <div
+            key={`event_card_${event.id}`}
+            className="relative aspect-video rounded-lg overflow-hidden"
+          >
+            <div className="absolute inset-x-0 bottom-0 p-2">
+              <h1 className="text-sm text-primary-foreground">{event.name}</h1>
+              <p className="text-xs text-secondary-foreground">
+                {event.locationId} - {event.date}
+              </p>
+            </div>
 
-      {events.map((event) => (
-        <div
-          key={`event_card_${event.id}`}
-          className="relative aspect-video rounded-lg overflow-hidden"
-        >
-          <div className="absolute inset-x-0 bottom-0 p-2">
-            <h1 className="text-sm text-primary-foreground">{event.name}</h1>
-            <p className="text-xs text-secondary-foreground">
-              {event.locationId} - {event.date}
-            </p>
+            <Image
+              className="object-cover h-full w-full"
+              src={event.imageUrl}
+              width={320}
+              height={200}
+              alt={event.name}
+            />
           </div>
-
-          <Image
-            className="object-cover h-full w-full"
-            src={event.imageUrl}
-            width={320}
-            height={200}
-            alt={event.name}
-          />
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </>
   );
 }
